aa4b5006c0843f8e753f947c48235e89aac3348b
Ryan O'Leary <ryanoleary26@gmail.com>
2022-03-02 21:15:58 +0000

    created header and footer components

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header(){
+    return(
+        <>
+            <section className="header">
+                <h1>Header</h1>
+            </section>
+        </>
+    )
+
+}
+
+
+export default Header;
