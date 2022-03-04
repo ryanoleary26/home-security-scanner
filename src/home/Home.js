2f92f8e314edff1b67881428ca61ccb5ffb0f4dc
Ryan O'Leary <ryanoleary26@gmail.com>
2022-03-04 20:55:03 +0000

    created home page

---
+++ src/home/Home.js
@@ -0,0 +1,14 @@
+// Style
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+        <section>
+          <h1>This is the home page</h1>
+        </section>
+    </>
+  );
+}
+
+export default Home;
