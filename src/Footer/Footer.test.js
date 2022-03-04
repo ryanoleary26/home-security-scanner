534f7ae010a7e7de4619cf48d33c7a0ca23a2fec
Ryan O'Leary <ryanoleary26@gmail.com>
2022-03-04 20:59:24 +0000

    repository reorganisation

--- src/Footer/Footer.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders the footer', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Footer/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/Header/Header.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders the header', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Header/i);
-  expect(linkElement).toBeInTheDocument();
-});
