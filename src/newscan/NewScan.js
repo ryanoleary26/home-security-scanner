7bbfce430b2fce222b1752dc1d03abae16ddd77f
Ryan O'Leary <ryanoleary26@gmail.com>
2022-03-04 20:52:21 +0000

    created NewScan page

---
+++ src/newscan/NewScan.js
@@ -0,0 +1,13 @@
+import './NewScan.css';
+
+function NewScan() {
+  return (
+    <>
+        <section>
+          <h1>This is the new scan page</h1>
+        </section>
+    </>  
+    );
+}
+
+export default NewScan;
