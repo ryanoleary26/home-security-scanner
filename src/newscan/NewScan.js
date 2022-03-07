c96728d7a7eb0b72ee9b44b13bededa7c1c78a35
Ryan O'Leary <ryanoleary26@gmail.com>
2022-03-07 22:00:59 +0000

    Added grid template

--- src/newscan/NewScan.js
+++ src/newscan/NewScan.js
@@ -1,11 +1,33 @@
+// Style
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './NewScan.css';
+
+// Components
+import Grid from '@mui/material/Grid'
+
 
 function NewScan() {
   return (
     <>
-        <section>
-          <h1>This is the new scan page</h1>
-        </section>
+      <Grid container sx={{paddingBottom: 30}}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <section>
+            <h1>New Scan</h1>
+            
+          </section>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          
+        </Grid>
+      </Grid>
     </>  
     );
 }
