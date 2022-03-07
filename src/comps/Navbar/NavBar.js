b179351861abea960255332167781b9ab02e3b93
Ryan O'Leary <ryanoleary26@gmail.com>
2022-03-07 22:08:44 +0000

    Adjusted navbar style/structure

--- src/comps/Navbar/NavBar.js
+++ src/comps/Navbar/NavBar.js
@@ -3,31 +3,28 @@ import './NavBar.css';
 
 // Components
 import {NavLink} from "react-router-dom";
+import Stack from '@mui/material/Stack';
 
-const NavBar = () => {
+function NavBar() {
     return(
-        <>
-            <nav>
-                <NavLink to="/">
-                    Home 
-                </NavLink>
-                <NavLink to="/newscan">
-                    New Scan
-                </NavLink>
-                <NavLink to="/scanschedule">
-                    Scan Schedules
-                </NavLink>
-                <NavLink to="/scanreports">
-                    Scan Reports
-                </NavLink>
-            </nav>
-        </>
-        
-    )
+            <div className="navbar">
+                <Stack className="nav" direction={{xs: "column", sm:"row"}} spacing={1}>
+                    <NavLink className="navitem" to="/">
+                        Home 
+                    </NavLink>
+                    <NavLink className="navitem" to="/newscan">
+                        New Scan
+                    </NavLink>
+                    <NavLink className="navitem" to="/schedules">
+                        Scan Schedules
+                    </NavLink>
+                    <NavLink className="navitem" to="/results">
+                        Scan Results
+                    </NavLink>
+                </Stack>
+            </div>
+            );
+
 }
-    
-
-
-
 
 export default NavBar;
