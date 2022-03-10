0752a5a8716891ba810f786deff55df2a01b494c
Ryan O'Leary <ryanoleary26@gmail.com>
2022-03-10 23:14:25 +0000

    prettified and removed unnecessary divs

--- src/results/Results.js
+++ src/results/Results.js
@@ -17,32 +17,40 @@ import Grid from '@mui/material/Grid';
 
 function createData(scanNo, scanDate, scanDev, scanVuln, scanReport) {
   return {
-    scanNo, scanDate, scanDev, scanVuln, scanReport,
+    scanNo,
+    scanDate,
+    scanDev,
+    scanVuln,
+    scanReport,
   };
 }
 
 // Replace with database query
 const rows = [
-  createData(1, '01/01/22', 3, 4, 'Full report'),
-  createData(2, '01/02/22', 6, 4, 'Full report'),
-  createData(3, '01/03/22', 10, 5, 'Full report'),
-  createData(4, '01/04/22', 11, 6, 'Full report'),
-  createData(5, '01/05/22', 10, 5, 'Full report'),
-  createData(6, '01/01/22', 3, 4, 'Full report'),
-  createData(7, '01/02/22', 6, 4, 'Full report'),
-  createData(8, '01/03/22', 10, 5, 'Full report'),
-  createData(9, '01/04/22', 11, 6, 'Full report'),
-  createData(10, '01/05/22', 10, 5, 'Full report'),
+  createData(1, '01/01/22', 3, 4, 'Full report '),
+  createData(2, '01/02/22', 6, 4, 'Full report '),
+  createData(3, '01/03/22', 10, 5, 'Full report '),
+  createData(4, '01/04/22', 11, 6, 'Full report '),
+  createData(5, '01/05/22', 10, 5, 'Full report '),
+  createData(6, '01/01/22', 3, 4, 'Full report '),
+  createData(7, '01/02/22', 6, 4, 'Full report '),
+  createData(8, '01/03/22', 10, 5, 'Full report '),
+  createData(9, '01/04/22', 11, 6, 'Full report '),
+  createData(10, '01/05/22', 10, 5, 'Full report '),
 ];
 
 function Results() {
   return (
     <Grid container sx={{ paddingBottom: 30 }}>
-      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+      <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
         <section>
           <h1>Scan Results</h1>
-          <TableContainer sx={{ maxWidth: 900, maxHeight: 400 }} component={Paper}>
-            <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableContainer sx={{ maxHeight: 400 }} component={Paper}>
+            <Table
+              stickyHeader
+              sx={{ minWidth: 650 }}
+              aria-label="simple table"
+            >
               <TableHead>
                 <TableRow>
                   <TableCell>Scan No.</TableCell>
@@ -66,8 +74,8 @@ function Results() {
                     <TableCell>{row.scanVuln}</TableCell>
                     <TableCell>
                       <a href="#a">
-                        { row.scanReport }
-                        -`&gt;`
+                        {row.scanReport}
+                        -&gt;
                       </a>
                     </TableCell>
                   </TableRow>
@@ -78,63 +86,59 @@ function Results() {
         </section>
       </Grid>
       <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-        <section className="keyResult">
-          <h1>Key Results</h1>
-        </section>
+        <h1>Key Results</h1>
       </Grid>
 
       <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-        <section className="keyResult">
-          <div>
-            <h2>Most common device types</h2>
-            <TableContainer sx={{ maxWidth: 900, maxHeight: 400 }} component={Paper}>
-              <Table stickyHeader aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Type</TableCell>
-                    <TableCell>Description</TableCell>
-                    <TableCell>Product Page</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>a</TableCell>
-                    <TableCell>a</TableCell>
-                    <TableCell>a</TableCell>
-                    <TableCell>a</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </div>
-        </section>
+        <h2>Most common device types</h2>
+        <TableContainer
+          sx={{ maxWidth: 900, maxHeight: 400 }}
+          component={Paper}
+        >
+          <Table stickyHeader aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Type</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Product Page</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>a</TableCell>
+                <TableCell>a</TableCell>
+                <TableCell>a</TableCell>
+                <TableCell>a</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
 
       <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-        <section className="keyResult">
-          <div>
-            <h2>Devices requiring attention</h2>
-            <TableContainer sx={{ maxWidth: 900, maxHeight: 400 }} component={Paper}>
-              <Table stickyHeader aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Type</TableCell>
-                    <TableCell>Action needed</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>a</TableCell>
-                    <TableCell>a</TableCell>
-                    <TableCell>a</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </div>
-        </section>
+        <h2>Devices requiring attention</h2>
+        <TableContainer
+          sx={{ maxWidth: 900, maxHeight: 400 }}
+          component={Paper}
+        >
+          <Table stickyHeader aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Type</TableCell>
+                <TableCell>Action needed</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>a</TableCell>
+                <TableCell>a</TableCell>
+                <TableCell>a</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </Grid>
   );
