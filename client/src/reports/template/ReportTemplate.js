781096f9b1399605bae6c185c1eda8639124218b
Ryan O'Leary <ryanoleary26@gmail.com>
2022-05-03 16:14:18 +0100

    Added tooltips to report information

--- client/src/reports/template/ReportTemplate.js
+++ client/src/reports/template/ReportTemplate.js
@@ -165,12 +165,13 @@ function Report(props) {
     <Grid container spacing={4} sx={{ paddingBottom: 5 }}>
       <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
         <h2>Quick Summary</h2>
+        <p>Hover over each cell for descriptions.</p>
       </Grid>
       <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
         <Table sx={{ '&:last-child tr': { backgroundColor: '#ffffff' } }}>
           <TableBody>
             <TableRow>
-              <TableCell variant="head">Nmap Version</TableCell>
+              <TableCell title="Version of the tool used for this scan." variant="head">Nmap Version</TableCell>
               <TableCell>{data.version.toString()}</TableCell>
             </TableRow>
             <TableRow>
@@ -191,7 +192,7 @@ function Report(props) {
               <TableCell>{data.scanID.toString()}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell variant="head">Hosts discovered</TableCell>
+              <TableCell title="The number of devices identified in this scan." variant="head">Hosts discovered</TableCell>
               <TableCell>{data.hosts.length}</TableCell>
             </TableRow>
           </TableBody>
@@ -200,6 +201,7 @@ function Report(props) {
 
       <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
         <h2>Host Summary</h2>
+        <p>Hover over each cell for descriptions.</p>
       </Grid>
       <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
         <Grid container spacing={3}>
@@ -211,28 +213,34 @@ function Report(props) {
                 <Table sx={{ tr: { backgroundColor: '#ffffff' }, marginBottom: 5 }}>
                   <TableBody>
                     <TableRow>
-                      <TableCell variant="head">IP Address</TableCell>
+                      <TableCell title="This is the address that the device uses to communicate across your home network." variant="head">IP Address</TableCell>
                       <TableCell>{host.ipAddress ? host.ipAddress : 'Not available'}</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell variant="head">MAC Address</TableCell>
+                      <TableCell title="The unique identifier for the device identified." variant="head">MAC Address</TableCell>
                       <TableCell>{host.mac ? host.mac : 'Not available'}</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell variant="head">Device Vendor</TableCell>
+                      <TableCell title="The likely manufacturer or vendor of the device." variant="head">Device Vendor</TableCell>
                       <TableCell>{host.vendor ? host.vendor : 'Not available'}</TableCell>
                     </TableRow>
 
                     <TableRow>
-                      <TableCell rowSpan={host.ports.length !== 0 ? host.ports.length + 1 : 2} variant="head">Open/Identified Ports</TableCell>
+                      <TableCell
+                        title="The ports and associated services that have been identified on this device."
+                        rowSpan={host.ports.length !== 0 ? host.ports.length + 1 : 2}
+                        variant="head"
+                      >
+                        Open/Identified Ports
+                      </TableCell>
                     </TableRow>
 
                     {host.ports.length !== 0 ? (host.ports.map((port) => (
                       <TableRow key={port.number}>
                         <TableCell>
                           <p>Port number: {port.number}</p>
-                          <p>Service: {port.service}</p>
-                          <p>Protocol: {port.protocol}</p>
+                          <p title={`The service that is found on port ${port.number}`}>Service: {port.service.toUpperCase()}</p>
+                          <p>Protocol: {port.protocol.toUpperCase()}</p>
                           <p><u>Vulnerability Data</u></p>
                           {
                             port.script !== undefined
